ded50b50840c00f15f6e1b709e08d8ef0b07389b
yuuki1293 <yuuki1293@outlook.jp>
2023-07-15 23:15:54 +0900

    chore: コメントを追加

--- src/main/scala/com/github/yuuki1293/parser/CParser.scala
+++ src/main/scala/com/github/yuuki1293/parser/CParser.scala
@@ -5,17 +5,40 @@ import scala.util.parsing.combinator.*
 import scala.util.parsing.input.Reader
 
 object CParser extends JavaTokenParsers {
-  def integerConstant: Parser[String ~ String ~ Option[String]] =
-    "[+-]?".r
-      ~ (decimalConstant | octalConstant | hexConstant)
+  /**
+   * 整数のパーサー。
+   *
+   * 正定数が符号なしであることを示すには、文字uあるいはUをあとに付けて良い。
+   * またそれがlongであることを示すのに文字lあるいはLを付けることができる。
+   */
+  def integerConstant: Parser[String ~ Option[String]] =
+      (decimalConstant | octalConstant | hexConstant)
       ~ opt("[uU]?[lL]?".r)
 
+  /**
+   * 10進数のパーサー。
+   *
+   * 0以外から始まる数字の列。
+   * 数字の列は0~9を含む。
+   */
   private def decimalConstant: Parser[String] =
     """0|[1-9]\d*""".r
 
+  /**
+   * 8進数のパーサー。
+   *
+   * 0から始まる数字の列。
+   * 数字の列は0~7を含む。
+   */
   private def octalConstant: Parser[String] =
     """0[0-7]+""".r
 
+  /**
+   * 16進数のパーサー。
+   *
+   * 0xあるいは0Xで始まる数字の列。
+   * 数字の列は0～9、a～fまたはA～Fを含む。
+   */
   private def hexConstant: Parser[String] =
     """0[xX][\da-fA-F]+""".r
 }
