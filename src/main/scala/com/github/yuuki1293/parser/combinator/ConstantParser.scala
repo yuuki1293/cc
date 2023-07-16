810c00d19a663eea6df4820b7c29fa966f44e282
yuuki1293 <yuuki1293@outlook.jp>
2023-07-16 17:45:21 +0900

    refactor: ConstantParserにまとめた

---
+++ src/main/scala/com/github/yuuki1293/parser/combinator/ConstantParser.scala
@@ -0,0 +1,110 @@
+package com.github.yuuki1293.parser.combinator
+
+import scala.util.matching.Regex
+import scala.util.parsing.combinator.RegexParsers
+
+object ConstantParser extends RegexParsers {
+  /**
+   * 整数リテラルのパーサー。
+   *
+   * 整定数が符号なしであることを示すには、文字uあるいはUをあとに付けて良い。
+   * またそれがlongであることを示すのに文字lあるいはLを付けることができる。
+   */
+  def integerConstant: Regex =
+    s"""($octal|$hex|$decimal)[uU]?[lL]?""".r
+
+  /**
+   * 10進数にマッチする。
+   *
+   * 0以外から始まる数字の列。
+   * 数字の列は0~9を含む。
+   */
+  private val decimal =
+    """0|[1-9]\d*"""
+
+  /**
+   * 8進数にマッチする。
+   *
+   * 0から始まる数字の列。
+   * 数字の列は0~7を含む。
+   */
+  private val octal =
+    """0[0-7]+"""
+
+  /**
+   * 16進数にマッチする。
+   *
+   * 0xあるいは0Xで始まる数字の列。
+   * 数字の列は0～9、a～fまたはA～Fを含む。
+   */
+  private val hex =
+    """0[xX][\da-fA-F]+"""
+
+  /**
+   * 浮動小数点リテラルをパースする。
+   *
+   * 整数部分、小数点、小数部分、eまたはE、整数の指数（符号は省略可）、および省略可能な型接尾子（f、F、l、Lのいずれか）からなる。
+   * 整数と小数はともに数字の列からなり、一方は省略可能。eと指数もしくは小数点の一方は省略可能。
+   */
+  def floatingConstant: Regex =
+    s"""($floating1|$floating2|$floating3)[fFlL]?""".r
+
+  /**
+   * 仮数部にマッチする。
+   *
+   * eまたはEの後に指数が続く。
+   */
+  private val mantissa =
+    """[eE][+-]?\d+"""
+
+  /**
+   * 整数部分、小数点、小数部分、仮数部にマッチする。
+   *
+   * 整数部分と仮数部は省略できる。
+   */
+  private val floating1 =
+    """\d*\.\d+(""" + mantissa + """)?"""
+
+  /**
+   * 整数部分、小数点、仮数部にマッチする。
+   *
+   * 仮数部は省略できる。
+   */
+  private val floating2 =
+    """\d+\.(""" + mantissa + """)?"""
+
+  /**
+   * 整数部分、仮数部にマッチする。
+   */
+  private val floating3 =
+    """\d+""" + mantissa
+
+  /**
+   * 文字リテラルをパースする。
+   *
+   * 文字定数は'x'のように単一引用符で囲まれた一つ以上の文字の列。
+   * 文字定数には'文字あるいは改行は含まれない。
+   */
+  def charConstant: Regex =
+    s"""L?'($escapedChar|$oneChar)'""".r
+
+  /**
+   * エスケープされた文字にマッチする。
+   * マッチするエスケープ列は以下の通り。
+   *
+   * \n、\t、\v、\b、\r、\f、\a、\\、\?、\'、\"、\ooo、\xhh
+   *
+   * エスケープ\oooは、バックスラッシュの後に1～3個の8進数を並べたもので、望む文字の値を表すのに使われる。
+   * エスケープ\xhhはバックスラッシュの後にx、さらに16進数を続けたもので、これも望む文字の値を表すのに使われる。
+   */
+  private val escapedChar =
+    """\\([ntvbrfa\\?'"]|[0-7]{1,3}|x[\da-fA-F]+)"""
+
+  /**
+   * 1文字にマッチする。
+   *
+   * NUL、LF、CR、\、'は文字リテラルに含まれない。
+   */
+  private val oneChar =
+    """[^\x00\f\n\\']"""
+}
---
+++ src/test/scala/com/github/yuuki1293/parser/combinator/ConstantParserSpec.scala
@@ -0,0 +1,113 @@
+package com.github.yuuki1293.parser.combinator
+
+import com.github.yuuki1293.parser.combinator.ConstantParser.*
+import org.scalatest.diagrams.Diagrams
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.util.parsing.combinator.RegexParsers
+
+class ConstantParserSpec extends AnyFlatSpec with Diagrams with RegexParsers {
+  "integerConstant" should "10進数をパースする。" in {
+    List(
+      "0",
+      "1234567890",
+      "1234567890u",
+      "1234567890l",
+      "1234567890ul"
+    )
+      .map(parseAll(integerConstant, _).successful)
+      .foreach(assert(_))
+  }
+
+  it should "8進数をパースする。" in {
+    List(
+      "00",
+      "012345670",
+      "012345670u",
+      "012345670l",
+      "012345670ul"
+    )
+      .map(parseAll(integerConstant, _).successful)
+      .foreach(assert(_))
+  }
+
+  it should "16進数をパースする。" in {
+    List(
+      "0x0",
+      "0x123aF",
+      "0x123aFu",
+      "0x123aFl",
+      "0x123aFul"
+    )
+      .map(parseAll(integerConstant, _).successful)
+      .foreach(assert(_))
+  }
+
+  it should "パースに失敗する。" in {
+    List(
+      "",
+      "ABC",
+      "09",
+      "0xG",
+      "u",
+      "5 u"
+    )
+      .map(!parseAll(integerConstant, _).successful)
+      .foreach(assert(_))
+  }
+
+  "charConstant" should "1文字をパースする。" in {
+    List(
+      """' '""",
+      """'a'""",
+      """'\n'""",
+      """'\\'""",
+      """'\0'""",
+      """'\x1F00'""",
+      "'\t'",
+      """L'あ'"""
+    )
+      .map(parseAll(charConstant, _).successful)
+      .foreach(assert(_))
+  }
+
+  it should "パースに失敗する。" in {
+    List(
+      "",
+      """''""",
+      """'ab'""",
+      """'\'""",
+      "'\n'",
+      """'\900'""",
+      """'''""",
+      "'\u0000'"
+    )
+      .map(!parseAll(charConstant, _).successful)
+      .foreach(assert(_))
+  }
+
+  "floatingConstant" should "少数にマッチする。" in {
+    List(
+      "31.41e-1f",
+      ".56e3l",
+      ".123",
+      "10.e5",
+      "12.",
+      "123e4"
+    )
+      .map(parseAll(floatingConstant, _).successful)
+      .foreach(assert(_))
+  }
+
+  it should "パースに失敗する。" in {
+    List(
+      "",
+      ".",
+      "e21",
+      "0",
+      "a"
+    )
+      .map(!parseAll(floatingConstant, _).successful)
+      .foreach(assert(_))
+  }
+}
